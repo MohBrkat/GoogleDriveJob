053da3b1a99c4728845c95ddbe82703d05041655
MohBrkat <mbarakat@itgsoftware.com>
2020-05-12 11:47:24 +0300

    fixes

---
+++ GoogleDriveJob/Models/ListProductsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GoogleDriveJob.Models
+{
+    public class ListProductsModel
+    {
+        [Display(Order = -6)]
+        public string InsertDate { get; set; }
+        [Display(Order = -5)]
+        public int ListProductID { get; set; }
+        [Display(Order = -4)]
+        public int ListID { get; set; }
+        [Display(Order = -3)]
+        public int ProductID { get; set; }
+        [Display(Order = -2)]
+        public string ListProductStatus { get; set; }
+        [Display(Order = -1)]
+        public int Quantity { get; set; }
+        [Display(Order = 0)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ GoogleDriveJob/Models/TotalRevenueModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace GoogleDriveJob.Models
+{
+    public class TotalRevenueModel
+    {
+        [Display(Order = -6)]
+        public string InsertDate { get; set; }
+        public int OrderId { get; set; }
+        public int? BuyerID { get; set; }
+        public string OrderTransaction { get; set; }
+        public string SourceType { get; set; }
+        public string DeliveryOption { get; set; }
+        public string SKU { get; set; }
+        public decimal ProductPrice { get; set; }
+        public decimal? TotalRevenue { get; set; }
+    }
+}
