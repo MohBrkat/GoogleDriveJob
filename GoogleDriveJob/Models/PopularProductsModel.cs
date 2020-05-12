38a2ff81f2f6ac1d73a08a4fc0fd2573c8f4da6d
MohBrkat <mbarakat@itgsoftware.com>
2020-05-12 11:31:50 +0300

    Change the project structure

---
+++ GoogleDriveJob/Models/PopularProductsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleDriveJob.Models
+{
+    public class PopularProductsModel
+    {
+        public string SKU { get; set; }
+        public string ProductTitle { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int ItemsAdded { get; set; }
+        public int ItemsBought { get; set; }
+    }
+}
